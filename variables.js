164de37872d76412259c61070a24ea21aca41268
Agape Dimas <agapedimas14@gmail.com>
2025-08-03 23:15:09 +0700

    Update variables.js

--- variables.js
+++ variables.js
@@ -5,11 +5,11 @@ const Variables =
 
 	AppIcon: "https://assets.agapedimas.com/icon_logo.ico",
 	AppTitle: "Drill",
-	AppAssets: this.Production ? "https://assets.agapedimas.com" : "http://localhost:1202",
+	AppAssets: (process.env.NODE_ENV == "production") ? "https://assets.agapedimas.com" : "http://localhost:1202",
 	
 	WebHost: "https://drill.agapedimas.com",
 	WebHomepage: "/home",
-	WebPing: this.Production ? this.WebHost + "/ping" : "http://localhost:7199/ping",
+	WebPing: (process.env.NODE_ENV == "production") ? this.WebHost + "/ping" : "http://localhost:7199/ping",
 }
 
 module.exports = Variables;
